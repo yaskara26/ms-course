8e143f697151d854eb03020d1ad79ffa6929b31e
Yaskara <yaskaraguimaraes@hotmail.com>
2025-07-10 18:25:19 -0300

    ResrTemplate

--- hr-payroll/src/main/java/br/com/santangelo/yaskara/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/br/com/santangelo/yaskara/hrpayroll/service/PaymentService.java
@@ -1,12 +1,28 @@
 package br.com.santangelo.yaskara.hrpayroll.service;
 
 import br.com.santangelo.yaskara.hrpayroll.entities.Payment;
+import br.com.santangelo.yaskara.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Payment getPayment(Long workerId, int days) {
-            return new Payment("Totoro", 500.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", "" + workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
