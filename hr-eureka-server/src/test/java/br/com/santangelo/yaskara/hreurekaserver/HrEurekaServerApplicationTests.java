f657ef80c1f889f2ac22d29ff878b4d8e1a8cd49
Yaskara <yaskaraguimaraes@hotmail.com>
2025-07-12 15:58:35 -0300

    Prokect hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/br/com/santangelo/yaskara/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santangelo.yaskara.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
