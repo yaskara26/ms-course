3f0ac2873feb6c993368a1900704f14bc33044c6
Yaskara <yaskaraguimaraes@hotmail.com>
2025-07-12 16:20:43 -0300

    Configuring Eureka server

--- hr-eureka-server/src/main/java/br/com/santangelo/yaskara/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/br/com/santangelo/yaskara/hreurekaserver/HrEurekaServerApplication.java
@@ -1,8 +1,11 @@
 package br.com.santangelo.yaskara.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
